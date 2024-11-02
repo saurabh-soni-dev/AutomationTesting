6cbdafc266775697775e780e07e31988be1ca17f
saurabh-soni-dev <saurabh.soni6408@gmail.com>
2024-11-02 22:19:55 +0530

    Testing initial setup

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import React, {FC} from 'react';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import Navigation from './src/navigation/Navigation';
+import {persistor, store} from './src/redux/store';
 
-const App = () => {
+const App: FC = () => {
   return (
-    <View>
-      <Text>First snapshot test</Text>
-    </View>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Navigation />
+      </PersistGate>
+    </Provider>
   );
 };
 
